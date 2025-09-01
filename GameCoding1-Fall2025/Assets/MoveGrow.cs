2ce64c880f4a0c3e3b626b4f277095f6ce49aeb4
yasmaniandevil <yasmine.batniji@gmail.com>
2025-09-01 01:20:03 -0400

    save

--- GameCoding1-Fall2025/Assets/MoveGrow.cs
+++ GameCoding1-Fall2025/Assets/MoveGrow.cs
@@ -14,10 +14,15 @@ public class MoveGrow : MonoBehaviour
     float minBoundary = 8;
     float maxBoundary = -8;
 
+    public Vector3 scaleUpDown = new Vector3 (3, 3, 3);
+
     // Start is called before the first frame update
     void Start()
     {
         square = GetComponent<Transform>();
+
+        //this is how just to change scale
+        square.localScale = new Vector3(3, 3, 3);
     }
 
     // Update is called once per frame
@@ -26,20 +31,29 @@ public class MoveGrow : MonoBehaviour
         //move
         square.position += new Vector3(moveSpeed * direction * Time.deltaTime, 0, 0);
 
-        if(square.position.x > minBoundary || square.position.x < maxBoundary)
+        square.localScale += scaleUpDown;
+
+        if(square.position.x < minBoundary || square.position.x > maxBoundary)
         {
             Debug.Log("passed it");
             direction *= -1;
 
-            if(square.localScale.y > 3)
+            //if square scale larger or equal to 3
+            //or less than or equal to -3
+            if(square.localScale.y >= 3 || square.localScale.y <= -3)
             {
-
+                Debug.Log("shrink");
+                //square.localScale = transform.localScale * -1;
+                //scaleUpDown = -scaleUpDown;
+                Debug.Log("checking the shrink");
             }
         }
 
-        //rotate
-        square.Rotate(0, 0, 20);
+        //rotate simple
+        //square.Rotate(0, 0, 20);
+        //rotate by speed
+        square.Rotate(Vector3.forward * 20 * Time.deltaTime);
 
-        square.localScale = new Vector3(3, 3, 3);
+        
     }
 }
