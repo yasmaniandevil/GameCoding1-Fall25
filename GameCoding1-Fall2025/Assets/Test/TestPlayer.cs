df0aa02f52f214b32d4fc60a25e61bca57c82707
yasmaniandevil <yasmine.batniji@gmail.com>
2025-09-21 21:24:59 -0400

    test scene for student

--- GameCoding1-Fall2025/Assets/Test/TestPlayer.cs
+++ GameCoding1-Fall2025/Assets/Test/TestPlayer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WASDMovement : MonoBehaviour
+public class TestPlayer : MonoBehaviour
 {
     public float speed = 5f;
     public float jumpForce = 5f;
@@ -11,14 +11,16 @@ public class WASDMovement : MonoBehaviour
     public float extraSpeed = 10;
     int jumpCount;
     public bool sprintOn = false;
+    public AudioSource play1sound;
+    public AudioSource play2sound;
 
     // Start is called before the first frame update
     void Start()
     {
-        /*A Rigidbody in Unity is a component that enables a GameObject 
-         * to be controlled by the physics engine. When a Rigidbody component 
-         * is added to a GameObject, that object can then be influenced by 
-         * forces such as gravity, receive forces and torque through scripting, 
+        /*A Rigidbody in Unity is a component that enables a GameObject
+         * to be controlled by the physics engine. When a Rigidbody component
+         * is added to a GameObject, that object can then be influenced by
+         * forces such as gravity, receive forces and torque through scripting,
          * and interact with other physics-enabled objects in the scene through collisions.*/
         rb2d = GetComponent<Rigidbody2D>();
         currentSpeed = speed;
@@ -29,7 +31,7 @@ public class WASDMovement : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        
+
         //if the player hits space
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -47,7 +49,7 @@ public class WASDMovement : MonoBehaviour
             sprintOn = true;
             rb2d.velocity = new Vector2(horizontalInput * currentSpeed, verticalInput * currentSpeed);
         }
-        
+
         {
             currentSpeed = speed;
             rb2d.velocity = new Vector2(horizontalInput * currentSpeed, verticalInput * currentSpeed);
@@ -59,7 +61,7 @@ public class WASDMovement : MonoBehaviour
     //solid objects that bump into each other
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+
         Debug.Log("I Hit: " + collision.gameObject.tag);
 
         //exercise Tag Check on Collision
@@ -69,10 +71,18 @@ public class WASDMovement : MonoBehaviour
             Debug.Log("Hit Wall");
             //currentSpeed = extraSpeed;
             //changeSpeed = true;
+            play1sound.Play();
+            //Destroy(collision.gameObject);
         }
 
-        
-        
+        if (collision.gameObject.CompareTag("sound2"))
+        {
+            play2sound.Play();
+            Destroy(collision.gameObject);
+        }
+
+
+
     }
 
     //objects that overlap
@@ -88,4 +98,5 @@ public class WASDMovement : MonoBehaviour
             transform.position = new Vector2(5, 8);
         }*/
     }
+
 }
